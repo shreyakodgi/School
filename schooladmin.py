bc23e808af0b74ecf1571aace16ab9551a61be7a
shreyakodgi <67737111+shreyakodgi@users.noreply.github.com>
2021-01-30 22:00:17 +0530

    creates school administration program

---
+++ schooladmin.py
@@ -0,0 +1,9 @@
+condition True
+white condition:
+student_info = Input("Enter some student Information: ") I
+print student Info)
+condition_check = input ("Enter (yes/no) if you want to enter information for another student: ")
+it condition check "yes":
+condition - True elif condition_check=
+condition = False
+"no" 
